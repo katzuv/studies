c23fb0ff0be6b84ee363bf21594d798ff10af004
Dan Katzuv <dan.dan.kafe@gmail.com>
2024-12-13 19:12:55 +0200

    Add homework 0 files

---
+++ Exercises/hw0/hw0.py
@@ -0,0 +1,10 @@
+'''
+This is a comment. It may span several lines.
+The interpreter will ignore everything here,
+including Python commands
+'''
+print("Explicit is _much_ better than implicit!") #this is also a comment
+print()
+print("NOW is better than *NEVER*.")
+print("Although ")
+print("never is often bet-ter than 'Right' now.")
