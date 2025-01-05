8ecad48071506c792f3709e3778c988f2e49c570
Dan Katzuv <dan.dan.kafe@gmail.com>
2025-01-05 19:52:43 +0200

    Add HW3Q2 solution

--- hw3/hw3q2.py
+++ hw3/hw3q2.py
@@ -4,11 +4,24 @@ for i in raw_numbers:
     numbers.append(int(i))
 target = int(input())
 
-for i in range(len(numbers)):
-    for j in range(i + 1, len(numbers)):
+combinations = []
+
+for i in range(len(numbers) - 2):
+    for j in range(i + 1, len(numbers) - 1):
         for k in range(j + 1, len(numbers)):
             first, second, third = numbers[i], numbers[j], numbers[k]
             if first == second or second == third or first == third:
                 continue
             if first + second + third == target:
-                print(f"{first}{second}{third}")
+                combinations.append(f"{first}{second}{third}")
+
+unique_combinations = []
+for first_index in range(len(combinations)):
+    for second_index in range(first_index + 1, len(combinations)):
+        if combinations[first_index] == combinations[second_index]:
+            break
+    else:
+        unique_combinations.append(combinations[first_index])
+
+for combination in unique_combinations:
+    print(combination)
