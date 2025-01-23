3f8ab7040db2418aff35e2ff5b774857f3be9274
Dan Katzuv <dan.dan.kafe@gmail.com>
2025-01-23 19:12:25 +0200

    Add HW4Q2#2 solution

--- Exercises/hw4/hw4q2.py
+++ Exercises/hw4/hw4q2.py
@@ -2,3 +2,14 @@ def applyPermutation(num_lst, perm_lst):
     list_copy = num_lst[:]
     for i, new_index in enumerate(perm_lst):
         num_lst[new_index] = list_copy[i]
+
+
+def getMatrix():
+    rows = int(input())
+    matrix = []
+    for _ in range(rows):
+        raw_row = input()
+        row = [int(number) for number in raw_row.split(",")]
+        matrix.append(row)
+
+    return matrix
