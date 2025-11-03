551c6d1c91a53a2215bdf0131d593aa728c6cf62
Dan Katzuv <dan.dan.kafe@gmail.com>
2025-11-03 22:51:48 +0200

    Calculate theoretical capacitance

--- 2026W/Lab/01 Workshop/capacitor.py
+++ 2026W/Lab/01 Workshop/capacitor.py
@@ -3,3 +3,8 @@ import scipy
 eps0 = scipy.constants.epsilon_0  # F/m
 plate_diameter = 18e-2  # m
 distance = 0.5e-3  # m
+
+theoretical_capacitance = (
+    eps0 * scipy.constants.pi * (plate_diameter**2) / (4 * distance)
+)
+print(f"{theoretical_capacitance=:.3e}")
