c9bd87bee20d272312bfed7a72e5f3d6469621f1
Dan Katzuv <dan.dan.kafe@gmail.com>
2025-11-03 22:39:19 +0200

    Try to add line of potential between the electrodes

--- 2026W/Lab/01 Workshop/potential.py
+++ 2026W/Lab/01 Workshop/potential.py
@@ -29,4 +29,16 @@ plt.colorbar()
 # `cmap`: color map.
 plt.contour(coord_x, coord_y, potential, np.linspace(-2, 2, 10), cmap="Accent")
 
+
+x_axis = np.linspace(-L, L, N)
+potential_1d = get_potential(x_axis, 0, a, C)
+
+plt.figure()
+plt.plot(x_axis, potential_1d, label="Potential at y=0")
+plt.xlabel("x")
+plt.ylabel("Potential")
+plt.title("Potential along x-axis (y=0)")
+plt.grid(True)
+plt.legend()
+
 plt.show()
