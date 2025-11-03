86be4cca17bf5e2cd80d5a743c14b00aae52ab9c
Dan Katzuv <dan.dan.kafe@gmail.com>
2025-11-03 21:41:49 +0200

    Import dependencies

---
+++ 2026W/Lab/01 Workshop/data_analysis.py
@@ -0,0 +1,6 @@
+import numpy as np  # math functions
+import pandas as pd  # handling data structures (loaded from files)
+import scipy  # scientific functions
+from scipy.optimize import curve_fit as cfit  # non-linear curve fitting
+from scipy.stats import linregress  # contains linregress (for linear regression)
+from sklearn.metrics import r2_score  # import function that calculates R^2 score
