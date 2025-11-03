503b42f13c900b1c9216632b8e87c382f12df79a
Dan Katzuv <dan.dan.kafe@gmail.com>
2025-11-03 22:28:00 +0200

    Add potential color mesh

--- 2026W/Lab/01 Workshop/potential.py
+++ 2026W/Lab/01 Workshop/potential.py
@@ -1,10 +1,28 @@
+import matplotlib.pyplot as plt
 import numpy as np  # math functions
-import pandas as pd  # handling data structures (loaded from files)
-from sklearn.metrics import r2_score  # import function that calculates R^2 score
 
 C = 1
 a = 1
 L = 3
 N = 100
 coord = np.linspace(-L, L, N)  # defines coordinates
+# Create x and y grid for all coordinates.
 coord_x, coord_y = np.meshgrid(coord, coord)
+
+
+def ln_argument(x, y, a):
+    return np.abs(np.hypot(x - a, y) / a)
+
+
+def get_potential(x, y, a, C):
+    return -C * np.log(ln_argument(x, y, -a)) + C * ln_argument(x, y, a)
+
+
+# Calculate potential at every coordinate.
+potential = get_potential(coord_x, coord_y, a, C)
+
+plt.figure()
+# Make a color for every coordinate based on the potential.
+plt.pcolormesh(coord_x, coord_y, potential)
+plt.colorbar()
+plt.show()
