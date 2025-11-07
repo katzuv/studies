7e4c9bf4c51a57828f99da4fa6b518a712f77474
Dan Katzuv <dan.dan.kafe@gmail.com>
2025-11-07 13:30:29 +0200

    Create lines and table

--- 2026W/Lab/00 Workshop/Waves/spectroscopy.py
+++ 2026W/Lab/00 Workshop/Waves/spectroscopy.py
@@ -1,5 +1,6 @@
 import numpy as np
 import pandas as pd
+from prettytable import PrettyTable
 
 data = pd.read_csv("spectroscopy.csv")
 
@@ -7,3 +8,10 @@ d = 1e-3 / 500
 angle = np.deg2rad(data.grating_angle)
 wavelengths = d * np.sin(angle)
 print(wavelengths * 1e9)
+
+lines = {
+    f"Line {n+1}": (data.grating_angle[n], wavelengths[n], data.color[n])
+    for n in range(len(angle))
+}
+table = PrettyTable(("Grating angle [deg]", "Grating wavelength [nm]", "Color"))
+table.add_row((data.grating_angle, wavelengths, data.color))
