fe5c31d6a4a221dbc08f8c40884b6723f54f9e4f
Dan Katzuv <dan.dan.kafe@gmail.com>
2025-11-07 16:27:10 +0200

    wtf

--- 2026W/Lab/00 Workshop/Waves/acousto_optic.py
+++ 2026W/Lab/00 Workshop/Waves/acousto_optic.py
@@ -11,11 +11,15 @@ plt.show()
 x0, x1 = 600, 800
 x = np.linspace(x0, x1, 400)
 y0, y1 = 400, 600
-y = image[y0, x0 : x0 + len(x), 0]
-plt.plot(x, y, label="Intensity")
+y_near = image[y0, x0 : x0 + len(x), 0]
+plt.plot(x, y_near, label="Intensity")
 
-peaks, _ = signal.find_peaks(y, distance=40)
-plt.plot(x[peaks], y[peaks], "^", label="Peaks")
+peaks, _ = signal.find_peaks(y_near, distance=40)
+spacings = np.diff(peaks)
+avg_dist = np.mean(spacings) * 5.2e-6
+v = avg_dist * 2079e6
+print(f"{v=}")
+plt.plot(x[peaks], y_near[peaks], "^", label="Peaks")
 
 plt.grid()
 plt.legend()
@@ -29,12 +33,18 @@ plt.show()
 x0, x1 = 750, 850
 x = np.linspace(x0, x1, 100)
 y0, y1 = 450, 550
-y = image[y0, x0 : x0 + len(x), 0]
-plt.plot(x, y, label="Intensity")
+y_far = image[y0, x0 : x0 + len(x), 0]
+plt.plot(x, y_far, label="Intensity_far")
 
-peaks, _ = signal.find_peaks(y, distance=4)
-plt.plot(x[peaks], y[peaks], "^", label="Peaks")
-
+peaks, _ = signal.find_peaks(y_far, distance=1)
+plt.plot(x[peaks], y_near[peaks], "^", label="Peaks")
+brightest = sorted(peaks, key=lambda p: y_far[p], reverse=True)[:2]
+distance = (brightest[1] - brightest[0]) * 5.2e-6
+v = (2079e6 * 0.3 * 632.8e-9) / distance
+print(f"{v=}")
 plt.grid()
 plt.legend()
 plt.show()
+
+
+# I don't know how to use FFT, we haven't studied this yet... also something in my data is off because the velocities I get make no sense.
