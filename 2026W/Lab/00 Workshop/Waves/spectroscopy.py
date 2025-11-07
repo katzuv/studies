1b5f9b6582c13c9bab0bba64f2bd59e142d04038
Dan Katzuv <dan.dan.kafe@gmail.com>
2025-11-07 13:26:14 +0200

    Calculate wavelengths

--- 2026W/Lab/00 Workshop/Waves/spectroscopy.py
+++ 2026W/Lab/00 Workshop/Waves/spectroscopy.py
@@ -1,3 +1,9 @@
+import numpy as np
 import pandas as pd
 
 data = pd.read_csv("spectroscopy.csv")
+
+d = 1e-3 / 500
+angle = np.deg2rad(data.grating_angle)
+wavelengths = d * np.sin(angle)
+print(wavelengths * 1e9)
