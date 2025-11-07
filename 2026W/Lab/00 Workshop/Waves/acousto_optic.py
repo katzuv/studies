9b84cba1c52870da79f79c01951a0d7f1a07abcc
Dan Katzuv <dan.dan.kafe@gmail.com>
2025-11-07 15:33:36 +0200

    Do it separately for both pictures bc I have no power

--- 2026W/Lab/00 Workshop/Waves/acousto_optic.py
+++ 2026W/Lab/00 Workshop/Waves/acousto_optic.py
@@ -3,21 +3,38 @@ import numpy as np
 from scipy import signal
 
 
-def process_image(image_path):
-    image = plt.imread(image_path)
+image = plt.imread("acousto_near.tif")
 
-    plt.imshow(image)
-    plt.show()
+plt.imshow(image)
+plt.show()
 
-    x0, x1 = 600, 800
-    x = np.linspace(x0, x1, 400)
-    y0, y1 = 400, 600
-    y = image[y0, x0 : x0 + len(x), 0]
-    plt.plot(x, y, label="Intensity")
+x0, x1 = 600, 800
+x = np.linspace(x0, x1, 400)
+y0, y1 = 400, 600
+y = image[y0, x0 : x0 + len(x), 0]
+plt.plot(x, y, label="Intensity")
 
-    peaks, _ = signal.find_peaks(y, distance=40)
-    plt.plot(x[peaks], y[peaks], "^", label="Peaks")
+peaks, _ = signal.find_peaks(y, distance=40)
+plt.plot(x[peaks], y[peaks], "^", label="Peaks")
 
-    plt.grid()
-    plt.legend()
-    plt.show()
+plt.grid()
+plt.legend()
+plt.show()
+
+image = plt.imread("acousto_far.tif")
+
+plt.imshow(image)
+plt.show()
+
+x0, x1 = 750, 850
+x = np.linspace(x0, x1, 100)
+y0, y1 = 450, 550
+y = image[y0, x0 : x0 + len(x), 0]
+plt.plot(x, y, label="Intensity")
+
+peaks, _ = signal.find_peaks(y, distance=4)
+plt.plot(x[peaks], y[peaks], "^", label="Peaks")
+
+plt.grid()
+plt.legend()
+plt.show()
