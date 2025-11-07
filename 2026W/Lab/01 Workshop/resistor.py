fd3afe08c7d72da7b57ef081dbfd5995e00e2891
Dan Katzuv <dan.dan.kafe@gmail.com>
2025-11-07 12:55:04 +0200

    Add function that calculates resistance

--- 2026W/Lab/01 Workshop/resistor.py
+++ 2026W/Lab/01 Workshop/resistor.py
@@ -4,3 +4,7 @@ def get_current(voltage2, resistance1):
 
 def get_resistor_voltage(voltage1, voltage2):
     return voltage1 - voltage2
+
+
+def get_resistor_resistance(voltage, current):
+    return voltage / current
