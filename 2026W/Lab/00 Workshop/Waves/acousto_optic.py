ef90ff6370bdca12d986373549434d1d1a957926
Dan Katzuv <dan.dan.kafe@gmail.com>
2025-11-07 15:04:19 +0200

    Revert "smth"
    This reverts commit fd9b1454f2ee69906cb7bfacdf28e4c56be21c29.

--- 2026W/Lab/00 Workshop/Waves/acousto_optic.py
+++ 2026W/Lab/00 Workshop/Waves/acousto_optic.py
@@ -1,18 +1,8 @@
 import matplotlib.pyplot as plt
-import numpy as np
 
 
 def process_image(image_path):
     image = plt.imread(image_path)
-
-    area = image[400:600, 600:800, :]
-    x = np.arange(area.shape[1])
-
-    y_intensity = area[:, :, :].mean(axis=2)
-    plt.plot(x, y_intensity)
-
-    plt.xlabel("Pixel X in selected area")
-    plt.xlabel("Average intensity (red channel)")
 
     plt.imshow(image)
     plt.show()
