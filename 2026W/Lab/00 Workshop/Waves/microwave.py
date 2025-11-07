63fbfbcb41310b1bf93428e1695e75e5f975eb81
Dan Katzuv <dan.dan.kafe@gmail.com>
2025-11-07 14:11:26 +0200

    Plot data

--- 2026W/Lab/00 Workshop/Waves/microwave.py
+++ 2026W/Lab/00 Workshop/Waves/microwave.py
@@ -1,6 +1,13 @@
+import numpy as np
 import pandas as pd
+from matplotlib import pyplot as plt
 
 data = pd.read_csv("microwave.csv")
 theta = np.deg2rad(data["theta(deg)"])
 intensity1 = data["power_1"]
 intensity2 = data["power_2"]
+
+for intensity in (intensity1, intensity2):
+    plt.polar(theta, intensity)
+
+plt.show()
