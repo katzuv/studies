e023a3f5ab460fc2cc1b5dc7d6c4173b484ec3b2
Dan Katzuv <dan.dan.kafe@gmail.com>
2025-11-07 15:04:49 +0200

    Plot intensity

--- 2026W/Lab/00 Workshop/Waves/acousto_optic.py
+++ 2026W/Lab/00 Workshop/Waves/acousto_optic.py
@@ -1,8 +1,19 @@
 import matplotlib.pyplot as plt
+import numpy as np
 
 
 def process_image(image_path):
     image = plt.imread(image_path)
 
     plt.imshow(image)
+    plt.show()
+
+    x0, x1 = 600, 800
+    x = np.linspace(x0, x1, 400)
+    y0, y1 = 400, 600
+    y = image[y0, x0 : x0 + len(x), 0]
+    plt.plot(x, y, label="Intensity")
+
+    plt.grid()
+    plt.legend()
     plt.show()
