ad68d63704c7175e0f15bf908d100b30de52fe56
Dan Katzuv <dan.dan.kafe@gmail.com>
2025-11-07 16:33:55 +0200

    Calculate amplitude

--- 2026W/Lab/00 Workshop/Waves/buzzers.py
+++ 2026W/Lab/00 Workshop/Waves/buzzers.py
@@ -1,3 +1,4 @@
+import numpy as np
 import pandas as pd
 
 data = pd.read_csv("buzzers.csv")
@@ -5,3 +6,9 @@ x1 = data["x1"]
 amplitude = data["Amplitude"]
 length = data["L (cm)"]
 frequency = data["f (Hz)"]
+L = 0.246
+
+
+def get_amplitude(x, a, lam, phi):
+    t = (np.pi / lam) * (2 * x - L) + phi / 2
+    return np.abs(2 * a * np.cos(t))
