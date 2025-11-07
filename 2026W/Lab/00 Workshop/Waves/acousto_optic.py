e166368c2c3ba1c299e7d6c98e3a68751234e147
Dan Katzuv <dan.dan.kafe@gmail.com>
2025-11-07 14:28:44 +0200

    Revert "Rotate image"
    This reverts commit 0e1c854cf1d10624562710b68947d7c619480b34.

--- 2026W/Lab/00 Workshop/Waves/acousto_optic.py
+++ 2026W/Lab/00 Workshop/Waves/acousto_optic.py
@@ -1,11 +1,8 @@
 import matplotlib.pyplot as plt
-from scipy import ndimage
 
 
 def process_image(image_path):
     image = plt.imread(image_path)
-
-    image = ndimage.rotate(image, 90, reshape=True)
 
     plt.imshow(image)
     plt.show()
