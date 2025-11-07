1820c2d4f9ffb6e2c17cb2c52c85aa6fc62c52db
Dan Katzuv <dan.dan.kafe@gmail.com>
2025-11-07 14:28:21 +0200

    Rotate image

--- 2026W/Lab/00 Workshop/Waves/acousto_optic.py
+++ 2026W/Lab/00 Workshop/Waves/acousto_optic.py
@@ -1,8 +1,11 @@
 import matplotlib.pyplot as plt
+from scipy import ndimage
 
 
 def process_image(image_path):
     image = plt.imread(image_path)
+
+    image = ndimage.rotate(image, 90, reshape=True)
 
     plt.imshow(image)
     plt.show()
