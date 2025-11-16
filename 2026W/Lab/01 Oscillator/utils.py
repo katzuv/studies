169debd972c3e97f1a44bea38b5ba008d1b91d51
dan <dan.k@campus.technion.ac.il>
2025-11-16 17:43:40 +0200

    Parse driven data

--- 2026W/Lab/01 Oscillator/utils.py
+++ 2026W/Lab/01 Oscillator/utils.py
@@ -68,3 +68,26 @@ def get_edited_data_path(path: Path, start_index: int) -> Path:
     new_path.write_text(data)
 
     return new_path
+
+def get_edited_driven_data_path(path: Path, start_index: int=1) -> Path:
+    data = path.read_text()
+    for replacement in (
+        ("\t\t", ","),
+        ("\t", ","),
+        (" count C count D", ""),
+        ("count A", "ticks_a"),
+        ("count B", "ticks_b"),
+        ("#", "line"),
+        ("time(s)", "time"),
+    ):
+        data = data.replace(replacement[0], replacement[1])
+
+    header = data.splitlines()[0]
+    data = f"{header}\n" + "\n".join(
+        line.removesuffix(",") for line in data.splitlines()[start_index:]
+    )  # Trim data before sine.
+
+    new_path = path.with_suffix(".csv")
+    new_path.write_text(data)
+
+    return new_path
