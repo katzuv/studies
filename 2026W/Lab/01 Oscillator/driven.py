1a82b03ada7cb79a107ca9b9bf6bab0abd765eeb
dan <dan.k@campus.technion.ac.il>
2025-11-16 17:50:47 +0200

    Add amplitude ratio model

--- 2026W/Lab/01 Oscillator/driven.py
+++ 2026W/Lab/01 Oscillator/driven.py
@@ -7,3 +7,8 @@ def normalize_ticks(ticks):
     mid = (peak_average + valley_average) / 2
     return ticks - mid
 
+
+def amplitude_ratio_model(frequency, spring_constant, mass, natural_frequency, tau):
+    under_root = (natural_frequency ** 2 - frequency ** 2) ** 2 + (frequency / tau) ** 2
+    return (spring_constant / mass) * 1 / np.root(under_root)
+
