9ab71fbeda592a06303ad43acbf8eb23acd5fd88
Dan Katzuv <dan.dan.kafe@gmail.com>
2025-12-21 19:21:55 +0200

    Fix

--- 2026W/Lab/03 Sound/phase_shift.py
+++ 2026W/Lab/03 Sound/phase_shift.py
@@ -4,11 +4,11 @@ from matplotlib import pyplot as plt
 
 from utils import propagate_error
 
-f = 4.15  # kHz
+f = 4.15 * 1000  # kHz
 zamzam_x = 70.1  # initial distance [cm] +- 2 mm
 dx = (
     np.array((73.1, 73.6, 74.1, 74.8, 75.5, 76.4, 77.1, 77.7, 78.5, 79.1)) - zamzam_x
-)  # distance [cm] +- 2 mm
+) / 100  # distance [cm] +- 2 mm
 phase = np.array(
     (102, 125, 133, 166, -170, -133, -103, -79.5, -38, -13)
 )  # phase shift [deg]
@@ -18,7 +18,7 @@ plt.figure(2)
 plt.errorbar(
     dx,
     phase / (2 * np.pi),
-    xerr=0.2,
+    xerr=0.2 / 100,
     yerr=phase_err / (2 * np.pi),
     fmt=".",
     label="Measurements",
@@ -33,19 +33,26 @@ print(f"r = {r:.3f}, R² = {r2:.3f}")
 plt.plot(dx, y / (2 * np.pi), label="Linear regression")
 
 plt.xlabel("Distance [cm]")
-plt.ylabel("Phase shift [radians]")
+plt.ylabel("Phase shift [pi radians]")
 plt.grid()
 plt.legend()
 plt.savefig("freq_response.svg", format="svg")
 plt.show()
 
+
 def lam_func(slope):
     return 2 * np.pi / slope
-lam = 2*np.pi/reg.slope
-lam_err = propagate_error(lam_func,(reg.slope, ), (reg.stderr, ))
+
+
+lam = 2 * np.pi / reg.slope
+lam_err = propagate_error(lam_func, (reg.slope,), (reg.stderr,))
 print(f"Wavelength of sound in air is {lam:.3f} ± {lam_err:.3f} cm")
+
+
 def v_func(lambd, freq):
-    return lambd * freq * 10
+    return lambd * freq
+
+
 v = v_func(lam, f)
-v_err = propagate_error(v_func, (lam, f), (lam_err, f_err))
-print(f"Speed of sound in air is {v:.3f} m/s")
+v_err = propagate_error(v_func, (lam, f), (lam_err, 17.82))
+print(f"Speed of sound in air is {v:.3f} ± {v_err:.3f} m/s")
